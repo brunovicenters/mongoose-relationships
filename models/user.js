2f0b29f15e479dc973f6f7e586aafab57a878027
brunovicenters <brunovicente.dev@gmail.com>
2023-07-03 11:20:24 -0300

    One to few

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/farmStand", {
+  .connect("mongodb://127.0.0.1:27017/relationshipDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -11,3 +11,48 @@ mongoose
   .catch((err) => {
     console.log("Connection error! " + err);
   });
+
+const userSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  addresses: [
+    {
+      _id: { _id: false },
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const makeUser = async () => {
+  const u = new User({
+    first: "Lorena",
+    last: "Potter",
+  });
+  u.addresses.push({
+    street: "123 Sesame St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+  const res = await u.save();
+  console.log(res);
+};
+
+const addAddresses = async (id) => {
+  const user = await User.findById(id);
+  user.addresses.push({
+    street: "99 3rd St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+  const res = await user.save();
+  console.log(res);
+};
+
+addAddresses("64a2d029ea5af221acfa3c81");
